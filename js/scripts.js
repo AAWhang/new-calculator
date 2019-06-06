492071410815e31b60e6e9e30aa98a83cde54859
Aaron Whang and Marcin szopik <marcin@>
2019-06-06 16:56:10 -0700

    made all functions read off one form

--- js/scripts.js
+++ js/scripts.js
@@ -9,19 +9,23 @@ function addstuff(number1, number2) {
 
 function substuff(number1, number2) {
   var result = number1 - number2;
-   $("#output2").text(result);
+   $("#output").text(result);
 };
 
 function multiply(number1, number2) {
   var result = number1 * number2;
-   $("#output3").text(result);
+   $("#output").text(result);
 };
 
 function divide(number1, number2) {
   var result = number1 / number2;
-   $("#output4").text(result);
+   $("#output").text(result);
 };
 
+function readnum(){
+  number1 = parseInt($("#num1").val());
+  number2 = parseInt($("#num2").val());
+};
 
 
 
@@ -30,29 +34,25 @@ $(document).ready(function() {
 
   $("form#math").submit(function(event) {
       event.preventDefault();
-      number1 = parseInt($("#num1").val());
-      number2 = parseInt($("#num2").val());
+      readnum();
       addstuff(number1, number2);
     });
 
     $("form#mathsub").submit(function(event) {
         event.preventDefault();
-        number1 = parseInt($("#num3").val());
-        number2 = parseInt($("#num4").val());
+        readnum();
         substuff(number1, number2);
       });
 
       $("form#mathmul").submit(function(event) {
           event.preventDefault();
-          number1 = parseInt($("#num5").val());
-          number2 = parseInt($("#num6").val());
+          readnum();
           multiply(number1, number2);
         });
 
         $("form#mathdiv").submit(function(event) {
             event.preventDefault();
-            number1 = parseInt($("#num7").val());
-            number2 = parseInt($("#num8").val());
+            readnum();
             divide(number1, number2);
           });
 
